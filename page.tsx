0195df0858e0f0528c2b6930a050f6595fbcfd1b
Aleeza012 <khanaleeza799@gmail.com>
2024-10-31 21:51:21 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,28 +1,39 @@
-import Link from "next/link";
+import Image from 'next/image';
+import { formatPrice } from './utils/helpers';
 
-export default function Home() {
+const Product = () => {
+  const product = {
+    id: 1,
+    name: "Sample Product",
+    price: 7.99 ,
+    
+    description: "This is a great product that you will love! Customize it with your name, enjoy home delivery straight to your doorstep. A perfect choice for anyone looking for quality and style! This is a great product that you will love!",
+    
+    imageUrl: "/picture.jpeg", 
+  };
+
   return (
-    <div>
-      <h4 className="bg-purple-500 font-bold text-center">Simple Resume</h4>
-      <ul className="flex justify-center bg-blue-300">
-        <Link href="/">
-          <li className="inline-block m-3">Home</li>
-        </Link>
-        <Link href="/about">
-          <li className="inline-block m-3">About</li>
-        </Link>
-        <Link href="/skills">
-          <li className="inline-block m-3">Skills</li>
-        </Link>
-        <Link href="/education">
-          <li className="inline-block m-3">Education</li>
-        </Link>
-        <Link href="/contact">
-          <li className="inline-block m-3">Contact</li>
-        </Link>
-      </ul>
-      <h3 className="text-center font-bold text-pink-600 p-4 bg-gray-300">Home Page</h3>
-      <h1 className="pl-40 text-center font-bold text-pink-900 text-7xl">---WELCOME TO MY PORTFOLIO---</h1>
-    </div>
+    <main className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-3xl font-bold mb-4 text-gray-800">{product.name}</h1>
+      <Image 
+        src={product.imageUrl} 
+        alt={product.name} 
+        width={500} 
+        height={300} 
+        className="rounded-lg shadow-lg mb-4" 
+      />
+      <p className="text-gray-600 mb-4 text-center">{product.description}</p>
+      <h2 className='text-blue-800 mb-5 text-center'><b>CONTACT US: 03198136003</b></h2>
+      <h2 className='text-blue-800 mb-5 text-center'><b>ON INSTAGRAM: Xyz account</b></h2>
+      <h2 className="text-2xl font-semibold text-green-600 mb-4">{formatPrice(product.price)}</h2>
+ <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300">
+        Add to Cart
+      </button>
+      <p className="mt-4 text-sm text-gray-500">Hurry up, limited stock available!</p>
+    </main>
   );
-}
+};
+
+export default Product;
+
+
